b57509bff04751ebe2c045f0b02e36f7590ee46a
adi <adirafli0112@gmail.com>
2025-08-14 02:34:38 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,7 @@
-import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-700 text-yellow-500">
-      <div className="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center">
-        {/* Logo */}
-        <div className="text-2xl font-sans">LOGO</div>
-
-        {/* DEKSTOP MENU */}
-          <ul className="hidden text-xl md:flex space-x-6 font-sans">
-            <li>
-              <Link to="/" className="hover:text-white">
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link to="/client" className="hover:text-white">
-                CLIENT
-              </Link>
-            </li>
-          </ul>
-        </div>
-    </nav>
+    <div>Navbar</div>
   )
 }
 
